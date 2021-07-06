4f2ff30b24738b55afde3216c7a35bcf3e8bf211
xjmackey <xjmackey@gmail.com>
2021-07-06 17:39:20 -0400

    hid api key

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@ const box= document.getElementById('weather-box');
 const temperature= document.getElementById('temperature');
 const weather= document.getElementById('weather');
 const icon= document.getElementById('icon');
-const search=document.getElementById('search');
 const btn=document.getElementById('btn');
 const city=document.getElementById('location');
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 /*btn.onclick=('click', handleBtn());*/
+
 
 async function handleBtn(){
     var name = await document.getElementById('search').value;
@@ -14,9 +15,11 @@ async function handleBtn(){
     setName(name);
 }
 
+
+
 async function setName(tname){
     var cityName=tname;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=' +API_KEY, {mode: "cors"})
     const w = await response.json();
     var t=w.name;
     var c=w.sys.country;
@@ -27,27 +30,25 @@ async function setName(tname){
 
 async function newSearch(tname){
     var cityName=tname;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
     const w = await response.json();
     var t=w.name;
     setWeather(getTemp(t));
     seticon(geticon(t));
     getDesc(t);
+    getFeels(t);
+    getHumidity(t);
+
+    getPressure(t);
 }
 
 async function getTemp(name){
     var cityName = name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
     const w = await response.json();
     var temp = w.main.temp;
-    var icon= w.weather[0].icon;
-    var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-    
-    var humidity = w.main.humidity;
-    var pressure = w.main.pressure;
     var max = w.main.temp_max;
     var min= w.main.mp_min;
-    var description= w.weather[0].description;
     console.log(w);
     return temp;search.value
 }
@@ -62,7 +63,7 @@ async function setWeather(temp){
 
 async function geticon(name){
     var cityName=name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
     const w = await response.json();
     var icon=w.weather[0].icon;
     var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
@@ -78,7 +79,7 @@ async function seticon(url){
 
 async function getDesc(name){
     var cityName=name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
     const w = await response.json();
     var desc = w.weather[0].description;
     weather.innerText=desc;
@@ -87,11 +88,34 @@ async function getDesc(name){
 
 async function getFeels(name){
     var cityName=name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
     const w = await response.json();
-    var feels = w.main.feels_like;
-    feels.innerText=desc;
+    var K = w.main.feels_like;
+    feels= (K - 273.15) * (9/5) + 32;
+   
+    var feel = document.createElement('p');
+    feel.innerText='feels like: '+ Math.round(feels) + '°';
+    weather.appendChild(feel);
+}
 
+async function getHumidity(name){
+    var cityName = name;
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const w = await response.json();
+    var humidity = w.main.humidity;
+    var humid = document.createElement('p');
+    humid.innerText='humidity: ' +humidity;
+    weather.appendChild(humid);
+}
+
+async function getPressure(name){
+    var cityName = name;
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const w = await response.json();
+    var pressure = w.main.pressure;
+    var press = document.createElement('p');
+    press.innerText='pressure: ' +pressure;
+    weather.appendChild(press);
 }
 
 /* get default values*/
@@ -99,4 +123,7 @@ setWeather(getTemp('nahant'));
 seticon(geticon('nahant'));
 setName('nahant');
 getDesc('Nahant');
+getFeels('Nahant');
+getHumidity('nahant');
+getPressure('Nahant');
 
