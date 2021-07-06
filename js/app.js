149a80e924756a67b535827025f3fd3d3ccdd3bc
xjmackey <xjmackey@gmail.com>
2021-07-06 17:45:41 -0400

    meh

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const weather= document.getElementById('weather');
 const icon= document.getElementById('icon');
 const btn=document.getElementById('btn');
 const city=document.getElementById('location');
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 /*btn.onclick=('click', handleBtn());*/
 
@@ -19,7 +18,7 @@ async function handleBtn(){
 
 async function setName(tname){
     var cityName=tname;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=' +API_KEY, {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
     const w = await response.json();
     var t=w.name;
     var c=w.sys.country;
@@ -30,7 +29,7 @@ async function setName(tname){
 
 async function newSearch(tname){
     var cityName=tname;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
     const w = await response.json();
     var t=w.name;
     setWeather(getTemp(t));
@@ -44,7 +43,7 @@ async function newSearch(tname){
 
 async function getTemp(name){
     var cityName = name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
     const w = await response.json();
     var temp = w.main.temp;
     var max = w.main.temp_max;
@@ -63,7 +62,7 @@ async function setWeather(temp){
 
 async function geticon(name){
     var cityName=name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
     const w = await response.json();
     var icon=w.weather[0].icon;
     var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
@@ -79,7 +78,7 @@ async function seticon(url){
 
 async function getDesc(name){
     var cityName=name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
     const w = await response.json();
     var desc = w.weather[0].description;
     weather.innerText=desc;
@@ -88,7 +87,7 @@ async function getDesc(name){
 
 async function getFeels(name){
     var cityName=name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
     const w = await response.json();
     var K = w.main.feels_like;
     feels= (K - 273.15) * (9/5) + 32;
@@ -100,7 +99,7 @@ async function getFeels(name){
 
 async function getHumidity(name){
     var cityName = name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
     const w = await response.json();
     var humidity = w.main.humidity;
     var humid = document.createElement('p');
@@ -110,7 +109,7 @@ async function getHumidity(name){
 
 async function getPressure(name){
     var cityName = name;
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+ API_KEY, {mode: "cors"})
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=f70dd531494e4c446db8acff3273ab2c', {mode: "cors"})
     const w = await response.json();
     var pressure = w.main.pressure;
     var press = document.createElement('p');
